d1fc103a8615ffe7591cd3a6e57ca2c7bcb9c2ea
Sumit <sanjay.sumit10@gmail.com>
2021-11-20 22:56:52 +0530

    admin page

---
+++ src/components/admin.jsx
@@ -0,0 +1,120 @@
+import React from "react"
+import './admin.css'
+
+
+export function InitAdmin() {
+            return (
+                <div className= 'container'>
+                    <h3 className='heading'>All Orders</h3>
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>Orders</th>
+                                <th>Customer</th>
+                                <th>Address</th>
+                                <th>Status</th>
+                                <th>Placed At</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Order Id</td>
+                                <td>Sumit Kumar</td>
+                                <td>Patna</td>
+                                <td>
+                                    <select>
+                                        <option>Placed</option>
+                                        <option>Confirmed</option>
+                                        <option>Shipped</option>
+                                        <option>Reached at Destination City</option>
+                                        <option>Delivered</option>
+                                        <option>Completed</option>
+                                    </select>
+                                </td>
+                                <td>11:32 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Order Id</td>
+                                <td>Sumit Kumar</td>
+                                <td>Patna</td>
+                                <td>
+                                    <select>
+                                        <option>Placed</option>
+                                        <option>Confirmed</option>
+                                        <option>Shipped</option>
+                                        <option>Reached at Destination City</option>
+                                        <option>Delivered</option>
+                                        <option>Completed</option>
+                                    </select>
+                                </td>
+                                <td>11:32 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Order Id</td>
+                                <td>Sumit Kumar</td>
+                                <td>Patna</td>
+                                <td>
+                                    <select>
+                                        <option>Placed</option>
+                                        <option>Confirmed</option>
+                                        <option>Shipped</option>
+                                        <option>Reached at Destination City</option>
+                                        <option>Delivered</option>
+                                        <option>Completed</option>
+                                    </select>
+                                </td>
+                                <td>11:32 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Order Id</td>
+                                <td>Sumit Kumar</td>
+                                <td>Patna</td>
+                                <td>
+                                    <select>
+                                        <option>Placed</option>
+                                        <option>Confirmed</option>
+                                        <option>Shipped</option>
+                                        <option>Reached at Destination City</option>
+                                        <option>Delivered</option>
+                                        <option>Completed</option>
+                                    </select>
+                                </td>
+                                <td>11:32 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Order Id</td>
+                                <td>Sumit Kumar</td>
+                                <td>Patna</td>
+                                <td>
+                                    <select>
+                                        <option>Placed</option>
+                                        <option>Confirmed</option>
+                                        <option>Shipped</option>
+                                        <option>Reached at Destination City</option>
+                                        <option>Delivered</option>
+                                        <option>Completed</option>
+                                    </select>
+                                </td>
+                                <td>11:32 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Order Id</td>
+                                <td>Sumit Kumar</td>
+                                <td>Patna</td>
+                                <td>
+                                    <select>
+                                        <option>Placed</option>
+                                        <option>Confirmed</option>
+                                        <option>Shipped</option>
+                                        <option>Reached at Destination City</option>
+                                        <option>Delivered</option>
+                                        <option>Completed</option>
+                                    </select>
+                                </td>
+                                <td>11:32 PM</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            )
+}
